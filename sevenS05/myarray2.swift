ce684828d2c2dd72a86ff8fe57061883666e9c68
sevenOS <sevenos@sevenOSs-Mac.local>
2017-06-20 19:10:03 +0800

    Hv1

--- sevenS05/myarray2.swift
+++ sevenS05/myarray2.swift
@@ -10,17 +10,10 @@ import Foundation
 
 
 func myarray2(){
-    //
-    //  main.swift
-    //  sevenS05
-    //
-    //  Created by user on 2017/6/9.
-    //  Copyright © 2017年 seven. All rights reserved.
-    //
-    
+   
    
     
-    //二維陣列
+    //*****二維陣列
     
     
     var a1 = [[1, 2, 3], [4, 5], [],[6, 7, 8, 9]]
@@ -35,18 +28,35 @@ func myarray2(){
     print(a1.description)
     
     
-    a1[3].replaceSubrange(1...2, with: [100,200,300,400])  //這邊是一個元素  只需一維陣列
+    a1[3].replaceSubrange(1...2, with: [100,200,300,400,500,600,700,800,900])  //這邊是一個元素  只需一維陣列
+    //a1[3]陣列內 的1-2位置之間 取代為新的陣列
     print(a1.description)
     
-    a1.replaceSubrange(1...2, with: [[100,200,300,400]])  //這邊是一個元素  只需一維陣列
+    a1.replaceSubrange(1...2, with: [[100,200,300,400                                                                                                                                                                                  ]])  //這邊是一個元素  只需一維陣列
+    //取代
+    print(a1.description)
+    a1.replaceSubrange(1...3, with: [[0,0,0]])  //取代１－３得陣列
     print(a1.description)
     
+    print("--------")
     for (ii, vv) in a1.enumerated(){  //外圈即為二維
         
         
         for (i, value) in vv.enumerated() {   ///內圈 一維
             print("\(ii): \(i) => \(value)") }
     }
+    
+    print("--------")
+    var a2 = [[000],[111],[222,333,444,555],[7589,6],[777,888]]
+    for (ii,vv) in a2.enumerated() {
+//    print("\(ii):\(vv)")
+        
+        for (i,v) in vv.enumerated() {
+         print("\(ii):\(i)=>\(v)")
+        }
+    
+    }
+    
     
     
 
---
+++ sevenS05/myfunc1.swift
@@ -0,0 +1,9 @@
+//
+//  myfunc1.swift
+//  sevenS05
+//
+//  Created by sevenOS on 2017/6/20.
+//  Copyright © 2017年 seven. All rights reserved.
+//
+
+import Foundation
